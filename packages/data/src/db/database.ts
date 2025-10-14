97554d5b99233ded5d20b8bef2866acbdfc910cd
akbarsahata <11588611+akbarsahata@users.noreply.github.com>
2025-10-14 14:45:29 +0700

    feat(packages/data): init data package

---
+++ packages/data/src/db/database.ts
@@ -0,0 +1,30 @@
+import { drizzle, DrizzleD1Database } from "drizzle-orm/d1";
+import {
+  destinationEvaluations,
+  linkClicks,
+  links,
+} from "../drizzle-out/schema";
+
+const schema = {
+  links,
+  linkClicks,
+  destinationEvaluations,
+};
+
+let db: DrizzleD1Database<typeof schema> | null = null;
+
+export function initDatabase(bindingDb: D1Database) {
+  db = drizzle(bindingDb, {
+    schema,
+  });
+}
+
+export function getDb() {
+  if (!db) {
+    throw new Error("Database not initialized");
+  }
+
+  return db;
+}
+
+export type Db = DrizzleD1Database<typeof schema>;
